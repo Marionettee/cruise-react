a4f6454f1cdf1f4cc7b1e81b7ea816c0d2f98dd6
Haonan <330764949@qq.com>
2019-05-18 23:06:49 +0800

    remove some private infos

--- src/common/footer/index.js
+++ src/common/footer/index.js
@@ -3,7 +3,8 @@ import './footer.scss'
 export const Footer = ()=>{
     return (
         <footer className='footer'>
-            <span>Copyright 2017</span><span className="blod">Thought</span>works
+            <span>Copyright 2019</span>
+            {/* <span className="blod">Thought</span>works */}
         </footer>
     )
 }
